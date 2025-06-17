f5fbbc706affaa659d20dbf585c394e3ae9442e3
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-06-17 17:03:24 +0500

    feat: change logo , favicon

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import React, { useState } from "react";
@@ -31,17 +32,11 @@ export default function Navbar() {
       <div className="container mx-auto flex items-center justify-between p-4">
         {/* Logo */}
         <Link href="/" className="flex items-center gap-2 group">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            id="Layer_1"
-            data-name="Layer 1"
-            viewBox="0 0 24 24"
-            width="512"
-            height="512"
-            className="fill-black dark:fill-white w-6 h-6 group-hover:scale-110 transition-transform"
-          >
-            <path d="m24,8.086l-4,4v-1.086c0-1.344-.888-2.484-2.108-2.865.693-.858,1.108-1.949,1.108-3.135,0-2.757-2.243-5-5-5-1.634,0-3.087.788-4,2.003-.913-1.216-2.366-2.003-4-2.003C3.243,0,1,2.243,1,5c0,1.186.416,2.277,1.108,3.135-1.22.381-2.108,1.521-2.108,2.865v13h20v-4.086l4,4v-15.828ZM14,2c1.654,0,3,1.346,3,3s-1.346,3-3,3-3-1.346-3-3,1.346-3,3-3Zm-8,0c1.654,0,3,1.346,3,3s-1.346,3-3,3-3-1.346-3-3,1.346-3,3-3Zm12,20H2v-11c0-.551.449-1,1-1h14c.551,0,1,.449,1,1v11Zm4-2.914l-2-2v-2.172l2-2v6.172Z" />
-          </svg>
+          <img
+            src="/logo.png"
+            alt="FilmMix Logo"
+            className="w-10 h-10 bg-transparent group-hover:scale-110 transition-transform"
+          />
           <span className="text-xl font-bold bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent group-hover:scale-105 transition-transform">
             FilmMix
           </span>
